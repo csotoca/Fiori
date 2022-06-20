bda6eb7d50caf0efa3f3f31c08d4fb09106842e9
csotoca <csotoca@deloitte.es>
2022-06-20 15:16:38 +0000

    First commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["trial/invoices/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"trial/invoices/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
